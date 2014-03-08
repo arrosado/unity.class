9e48b8db6024083516001c3c5b228cbd1ae7f330
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2014-03-08 12:06:17 -0400

    Add menu to the StartMenu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+	public Texture backgroundTexture;
+
+	void OnGUI()
+	{
+		// Display our background texture.
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), this.backgroundTexture);
+
+		float left = Screen.width * .5f;
+		float top  = Screen.height * .5f;
+		float width = Screen.width * .5f;
+		float height = Screen.height * .1f;
+
+		float lastPos = 0;
+
+		foreach (var menuText in (new List<string>() { "Play Game", "Level1", "Level2", "Level3" })) {
+
+			if (GUI.Button (new Rect (left, top + (lastPos += height), width, height), menuText)) {
+				print ("Clicked " + menuText);		
+			}
+
+		}
+
+	}
+}
